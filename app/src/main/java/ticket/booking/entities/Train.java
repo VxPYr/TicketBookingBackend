cda6a69c55e26de6caebfc1d8cbc19ac698867cb
VxPYr <vishnujaswal2004@gmail.com>
2025-08-20 21:02:06 +0530

    Pushing gradle files and class entities and some folder structure

---
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -0,0 +1,17 @@
+package ticket.booking.entities;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class Train {
+    private String trainId;
+    private String trainNumber;
+    private String trainName;
+    private Date departureTime;
+    private Date arrivalTime;
+    private List<List<Boolean>> seats;
+    private Map<String, Time> stationTimes;
+    private List<String> stations;
+}

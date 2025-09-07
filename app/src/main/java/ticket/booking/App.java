10d3d870356cdb5ceed03f4e67e253f95c6d10fd
VxPYr <vishnujaswal2004@gmail.com>
2025-09-07 12:27:46 +0530

    test

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -5,13 +5,18 @@ package ticket.booking;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class App {
 
     public static void main(String[] args) {
         List<Integer> l = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        List<Integer> l1 = l.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        List<Integer> l1 = l.stream().filter(isEven()).collect(Collectors.toList());
         System.out.println(l1);
+    }
+
+    public static Predicate<Integer> isEven() {
+        return x -> x % 2 == 0;
     }
 }
